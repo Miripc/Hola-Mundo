d6dd5476db9f4ebd98c678a2b93fb21540a08a83
Miriam Plaza <miriampch07@gmail.com>
2024-07-08 12:12:02 +0200

    se integró la clase Mensaje

--- src/com/acne/HolaMundo.java
+++ src/com/acne/HolaMundo.java
@@ -2,6 +2,9 @@ package com.acne;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solicitaDatos("Escribe tu nombre: ");
+        msg.saludar(nombre);
+
     }
 }
