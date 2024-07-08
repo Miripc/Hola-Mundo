830ee7390b4a8a2c8fe4293953d46397dada61cf
Miriam Plaza <miriampch07@gmail.com>
2024-07-08 16:40:57 +0200

    Se reemplaza la clase persona por el registro persona

--- src/com/acne/Mensaje.java
+++ src/com/acne/Mensaje.java
@@ -7,8 +7,6 @@ public class Mensaje {
         System.out.println(mensaje);
         Scanner scn = new Scanner(System.in);
         return scn.nextLine();
-
-
     }
 
     public void saludar(Persona p) {
